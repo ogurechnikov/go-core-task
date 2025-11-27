6b33ecdde5c3ccae860a096b5e6c43f891ff219b
Sergey Ogurechnikov <ogurechnikov@gmail.com>
2025-11-27 16:17:22 +0700

    Добавил метод Remove, который удалет значение по ключу

--- 3/main_3.go
+++ 3/main_3.go
@@ -14,8 +14,15 @@ func main() {
 	m.Add("папа", 2)
 	m.Add("дедушка", 3)
 	fmt.Println("StringIntMap после добавления:", m)
+
+	m.Remove("папа")
+	fmt.Println("StringIntMap после удаления:", m)
 }
 
 func (s StringIntMap) Add(key string, value int) {
 	s[key] = value
+}
+
+func (s StringIntMap) Remove(key string) {
+	delete(s, key)
 }
